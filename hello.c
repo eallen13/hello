32b5eba04358b85ca77fdc6db888d5a2681e0ad8
Ellie Allen <eallen13@huskers.unl.edu>
2021-08-26 12:53:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Ellie Allen
+*  Date: 2021/08/26
+*
+* Hack 1 
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Ellie Allen \n");
+
+    return 0;
+}
